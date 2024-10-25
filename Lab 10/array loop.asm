3f3d6dbe19ddadaa8f82ac7d16c118e346f3625e
Hamna Qasim <hamnaqasim1243@gmail.com>
2024-10-25 20:59:12 +0500

    Lab 10
    COAL Lab 100 Arrays

---
+++ Lab 10/array loop.asm
@@ -0,0 +1,26 @@
+.model small
+.stack 100h
+.data 
+arr DB 49,50,51,52,53 
+.code
+main proc 
+    mov ax, @data  
+    mov ds, ax
+    mov si, offset arr
+    mov cx, 5
+    
+    loopp:
+    mov dl,[si]
+    mov ah,2
+    int 21h
+    
+    inc si
+    loop loopp
+    
+    
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
+
+
---
+++ Lab 10/reverse array.asm
@@ -0,0 +1,28 @@
+.model small
+.stack 100h
+.data 
+arr DB 49,50,51,52,53 
+.code
+main proc 
+    mov ax, @data  
+    mov ds, ax
+    mov si, offset arr
+    
+    
+    mov cx, 5
+    
+    loopp:
+    mov dl,[si+4]
+    mov ah,2
+    int 21h
+    
+    dec si
+    loop loopp
+    
+    
+    mov ah, 4ch
+    int 21h
+    main endp
+end main
+
+
